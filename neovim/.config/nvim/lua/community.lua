4b76ce238204c9018fb50433e31a4a3988fae0a5
Rahul N Akmol <rahulnakmol@gmail.com>
2024-07-14 21:44:13 +0530

    Added base configuration for neovim, alacritty tmux and zsh bindings

---
+++ neovim/.config/nvim/lua/community.lua
@@ -0,0 +1,37 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  -- Colorschemes
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.colorscheme.tokyodark-nvim" },
+
+  -- Enhancements
+  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  { import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+
+  -- Completions
+  { import = "astrocommunity.completion.codeium-nvim" },
+  -- -- Language Packs
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.scala" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -5,3 +5,11 @@ source <(fzf --zsh)
 eval "$(starship init zsh)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/node@20/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@20/include"
+eval "$(zoxide init zsh)"
+export BAT_THEME=Dracula
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
